1a5e4f51d9e9a89cc6988ffd344793b277c1b267
naha <haodn.work@gmail.com>
2025-07-21 11:18:44 +0700

    video18

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
 import React from "react";
 
 class App extends React.Component {
