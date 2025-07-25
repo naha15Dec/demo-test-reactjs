8cf1d51e2ebb51be83980a484e4ccd94bd903945
naha <haodn.work@gmail.com>
2025-07-25 12:12:55 +0700

    video46

--- src/App.js
+++ src/App.js
@@ -1,47 +1,7 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import React from "react";
-import MyComponent from "./components/MyComponent";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
-}
-
-// const App = () => {
-//   return (
-//     <>
-//       <div>Hello World w Naha</div>
-//       <MyComponent></MyComponent>{" "}
-//       {/* Gọi component như một thẻ tag HTML đặc biệt */}
-//     </>
-//   );
-// };
-
-// const App = () => {
-//   const count = useSelector((state) => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// };
+const App = () => {
+  return <div className="app-container">Hello world</div>;
+};
 
 export default App;
