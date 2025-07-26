2c48299e3066ac6ffe5768444d5ab307f787b414
naha <haodn.work@gmail.com>
2025-07-26 17:59:16 +0700

    video53

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -6,6 +6,18 @@ const HomePage = (props) => {
       <video autoPlay muted loop>
         <source src={videoHomePage} type="video/mp4" />
       </video>
+      <div className="homepage-content">
+        <h1 className="homepage-title">
+          Kiểm tra kiến thức của bạn cùng NahaQuiz!
+        </h1>
+        <p className="homepage-desc">
+          Thử sức với hàng trăm câu hỏi đa dạng về nhiều chủ đề: Công nghệ, Khoa
+          học, Lịch sử, Nghệ thuật và hơn thế nữa.
+        </p>
+        <div className="homepage-btn">
+          <button>Làm Quiz ngayyy!</button>
+        </div>
+      </div>
     </div>
   );
 };
