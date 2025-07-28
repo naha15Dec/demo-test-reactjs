b3345c71e5c0ad06b285043d6920d10142de312e
naha <haodn.work@gmail.com>
2025-07-28 23:20:20 +0700

    video61

---
+++ src/components/Admin/Content/TableUser.js
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import { getAllUsers } from "../../../Services/apiServices";
+const TableUser = () => {
+  const [listUsers, setListUsers] = useState([
+    // { id: 8, username: "nhkun", email: "naha1512@gmail.com", role: "USER" },
+    // { id: 9, username: "naha", email: "nahakun@gmail.com", role: "ADMIN" },
+  ]);
+
+  useEffect(() => {
+    fetchListUser();
+  }, []);
+
+  const fetchListUser = async () => {
+    let res = await getAllUsers();
+    if (res.EC === 0) {
+      setListUsers(res.DT);
+    }
+  };
+  return (
+    <>
+      <table className="table table-hover table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Username</th>
+            <th scope="col">Email</th>
+            <th scope="col">Role</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        {/* <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>{listUsers.username}</td>
+            <td>{listUsers.email}</td>
+            <td>{listUsers.role}</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+        </tbody> */}
+        <tbody>
+          {listUsers &&
+            listUsers.length > 0 &&
+            listUsers.map((user, index) => {
+              return (
+                <tr key={`table-user-${index}`}>
+                  <td>{index + 1}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                  <td>{user.role}</td>
+                  <td>
+                    <button className="btn btn-primary">View</button>
+                    <button className="btn btn-warning mx-3">Update</button>
+                    <button className="btn btn-danger">Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
+          {listUsers && listUsers.length === 0 && (
+            <tr>
+              <td colSpan={"4"}>Not found user</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default TableUser;
