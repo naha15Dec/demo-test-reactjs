54a8f9c75502be46a52141b9a4b66d8a9250c1ff
naha <haodn.work@gmail.com>
2025-08-04 16:05:03 +0700

    video83

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../Services/apiServices";
+import _ from "lodash";
 
 const DetailQuiz = () => {
   const params = useParams();
@@ -11,7 +12,32 @@ const DetailQuiz = () => {
   }, [quizId]);
   const fecthQuestion = async () => {
     let res = await getDataQuiz(quizId);
-    console.log(">>> check quesstion,", res);
+    if (res && res.EC === 0) {
+      let raw = res.DT;
+      let data = _.chain(raw)
+        // Group the elements of Array based on `color` property
+        .groupBy("id")
+        // `key` is group's name (color), `value` is the array of objects
+        .map((value, key) => {
+          let answers = [];
+          let questionDecription,
+            image = null;
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionDecription = item.description;
+              image = item.image;
+            }
+            answers.push(item.answers);
+          });
+          return {
+            questionId: key,
+            answers: answers,
+            image: image,
+            questionDecription: questionDecription,
+          };
+        })
+        .value();
+    }
   };
 
   return <div className="detail-quiiz-container">DetailQuiz Container</div>;
